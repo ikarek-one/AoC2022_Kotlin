4508fdab4d26709652846f877439c20300f9303b
flyingdutchgirl <80489316+flyingdutchgirl@users.noreply.github.com>
2022-12-10 00:50:40 +0100

    Day 9, part 2

--- src/main/kotlin/tasks/day09/RopeBridge.kt
+++ src/main/kotlin/tasks/day09/RopeBridge.kt
@@ -12,19 +12,12 @@ class RopeBridge : Task {
 
     override fun part1(lines: Sequence<String>): Long {
         val moves = lines.filter { it.isNotBlank() }.toList()
-
         val head = RopePart()
         val tail = RopePart()
 
         for (move in moves) {
             val tokens = move.split("\\s+".toRegex())
-            val direction = when (val cmd = tokens[0]) {
-                "R" -> 1 to 0
-                "L" -> -1 to 0
-                "U" -> 0 to 1
-                "D" -> 0 to -1
-                else -> throw Exception("Only letters R, L, U, D expected but found text = '$cmd'")
-            }
+            val direction = getDirectionFromTokens(tokens)
 
             repeat(tokens[1].toInt()) {
                 head.moveBy(direction)
@@ -34,29 +27,61 @@ class RopeBridge : Task {
         return tail.visitedPoints.distinct().size.toLong()
     }
 
-}
+    override fun part2(lines: Sequence<String>): Long {
+        val ropeLength = 10
 
-data class RopePart(var x: Int = INITIAL_POINT.first, var y: Int = INITIAL_POINT.second) {
-    val visitedPoints = mutableListOf(INITIAL_POINT)
+        val moves = lines.filter { it.isNotBlank() }.toList()
+        val head = RopePart()
+        val tails = List(ropeLength - 1) { RopePart() }
 
-    fun moveTowards(head: RopePart) {
-        val deltaX = head.x - this.x
-        val deltaY = head.y - this.y
+        for (move in moves) {
+            val tokens = move.split("\\s+".toRegex())
+            val direction = getDirectionFromTokens(tokens)
 
-        if (deltaX.absoluteValue > 1 || deltaY.absoluteValue > 1) {
-            this.x += deltaX.sign
-            this.y += deltaY.sign
+            repeat(tokens[1].toInt()) {
+                head.moveBy(direction)
+                var pseudoHead = head
+                for (i in 0..tails.lastIndex) {
+                    val tl = tails[i]
+                    tl.moveTowards(pseudoHead)
+                    pseudoHead = tl
+                }
+            }
         }
-        visitedPoints.add(this.x to this.y)
+        return tails.last().visitedPoints.distinct().size.toLong()
     }
 
-    fun moveBy(move: Pair<Int, Int>) {
-        this.x += move.first
-        this.y += move.second
+    private fun getDirectionFromTokens(tokens: List<String>) = when (val cmd = tokens[0]) {
+        "R" -> 1 to 0
+        "L" -> -1 to 0
+        "U" -> 0 to 1
+        "D" -> 0 to -1
+        else -> throw Exception("Only letters R, L, U, D expected but found text = '$cmd'")
     }
 
-    companion object {
-        val INITIAL_POINT: Pair<Int, Int> = 0 to 0
+
+    private data class RopePart(var x: Int = INITIAL_POINT.first, var y: Int = INITIAL_POINT.second) {
+        val visitedPoints = mutableListOf(INITIAL_POINT)
+
+        fun moveTowards(head: RopePart) {
+            val deltaX = head.x - this.x
+            val deltaY = head.y - this.y
+
+            if (deltaX.absoluteValue > 1 || deltaY.absoluteValue > 1) {
+                this.x += deltaX.sign
+                this.y += deltaY.sign
+            }
+            visitedPoints.add(this.x to this.y)
+        }
+
+        fun moveBy(move: Pair<Int, Int>) {
+            this.x += move.first
+            this.y += move.second
+        }
+
+        companion object {
+            val INITIAL_POINT: Pair<Int, Int> = 0 to 0
+        }
     }
 }
 
