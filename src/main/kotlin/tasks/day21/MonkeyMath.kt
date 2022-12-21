1bdd1be50fd39a347c621e15011558f4f3a8c6a6
flyingdutchgirl <80489316+flyingdutchgirl@users.noreply.github.com>
2022-12-21 22:44:24 +0100

    Day 21

--- src/main/kotlin/tasks/day21/MonkeyMath.kt
+++ src/main/kotlin/tasks/day21/MonkeyMath.kt
@@ -1,13 +1,11 @@
 package tasks.day21
 
 import base.Task
-import utils.fromConsolePartTwo
 import utils.testAssertions
 
 
 fun main() {
-    fromConsolePartTwo(MonkeyMath())
-//    testAssertions(::MonkeyMath)
+    testAssertions(::MonkeyMath)
 }
 
 class MonkeyMath : Task {
@@ -26,7 +24,6 @@ class MonkeyMath : Task {
         return map[wantedMonkey]!!()
     }
 
-    // part two passes the test but doesn't pass the real input - grateful for any hints why!
     override fun part2(lines: Sequence<String>): Long {
         val map = mutableMapOf<String, MonkeyVal>()
 
@@ -35,56 +32,30 @@ class MonkeyMath : Task {
                 parsePartTwo(it, map)
             }
 
-//        println("textAnswer = '${map["root"]?.textAnswer(map)}' \n")
-//        println("revTextAnswer = '${map["root"]?.reversedTextAnswer(map)}' \n")
-
         val root = map["root"]!!
         val (left, right) = root.monkey1 to root.monkey2
 
-        if (map[left]!!.isHumanInSubtree(map)) {
+        return if (map[left]!!.isHumanInSubtree(map)) {
             val seed = map[right]!!.answer(map)
-            return solve(map[left]!!, map, seed)
+            solvePartTwo(map[left]!!, seed, map)
         } else {
             map[MonkeyVal.humanName] = MonkeyVal(MonkeyVal.humanName, map[left]!!.answer(map))
             val seed = map[left]!!.answer(map)
-            return solve(map[right]!!, map, seed)
+            solvePartTwo(map[right]!!, seed, map)
         }
     }
 
 
-    fun solve(root: MonkeyVal, map: MutableMap<String, MonkeyVal>, seed: Long): Long {
-        if (root.name == MonkeyVal.humanName) {
-            return seed
-        }
-        val left = root.left(map)!!
-        val right = root.right(map)!!
-
-
-        if (left.isHumanInSubtree(map)) {
-            val newSeed = root.operation!!.reversed().function(
-                seed,
-                right.answer(map)
-            )
-            return solve(left, map, newSeed)
-        } else {
-            val newSeed = root.operation!!.reversed().function(
-                seed,
-                left.answer(map)
-            )
-            return solve(right, map, newSeed)
-        }
-    }
-
-    fun parseLine(line: String, map: MutableMap<String, () -> Long>) {
+    private fun parseLine(line: String, map: MutableMap<String, () -> Long>) {
         val numberRegex = "\\s?[+-]?\\d+\\s?".toRegex()
 
         val key = line.substringBefore(":")
         val valuePart = line.substringAfter(":")
 
+        @Suppress("RedundantLambdaArrow")
         val function = if (valuePart.matches(numberRegex)) {
             { -> valuePart.replace("\\s+".toRegex(), "").toLong() }
         } else {
-//            println("valPart = '$valuePart'")
             val (monkeyOne, operand, monkeyTwo) = valuePart.split("\\s+".toRegex()).filter { it.isNotBlank() }
             when (operand) {
                 "+" -> { -> map[monkeyOne]!!() + map[monkeyTwo]!!() }
@@ -95,10 +66,10 @@ class MonkeyMath : Task {
             }
         }
 
-        map.put(key, function)
+        map[key] = function
     }
 
-    fun parsePartTwo(line: String, map: MutableMap<String, MonkeyVal>) {
+    private fun parsePartTwo(line: String, map: MutableMap<String, MonkeyVal>) {
         val numberRegex = "\\s?[+-]?\\d+\\s?".toRegex()
 
         val key = line.substringBefore(":")
@@ -110,7 +81,7 @@ class MonkeyMath : Task {
             val (monkeyOne, operand, monkeyTwo) = valuePart.split("\\s+".toRegex()).filter { it.isNotBlank() }
             val operation = when (operand) {
                 "+" -> Operation.ADDITION
-                "-" -> Operation.SUBSTRACTION
+                "-" -> Operation.SUBTRACTION
                 "*" -> Operation.MULTIPLICATION
                 "/" -> Operation.DIVISION
                 else -> throw Exception("Unexpected operand found: '$operand'")
@@ -119,11 +90,38 @@ class MonkeyMath : Task {
             MonkeyVal(key, monkeyOne, monkeyTwo, operation)
         }
 
-        map.put(key, monkeyVal)
+        map[key] = monkeyVal
+    }
+
+    private fun solvePartTwo(root: MonkeyVal, seed: Long, map: MutableMap<String, MonkeyVal>): Long {
+        if (root.name == MonkeyVal.humanName) {
+            return seed
+        }
+
+        val left = root.left(map)!!
+        val right = root.right(map)!!
+
+        if (left.isHumanInSubtree(map)) {
+            val newSeed = root.operation!!.reversed().function(
+                seed,
+                right.answer(map)
+            )
+            return solvePartTwo(left, newSeed, map)
+        } else {
+            val newSeed = when (root.operation!!) {
+                Operation.SUBTRACTION -> left.answer(map) - seed
+                Operation.DIVISION -> left.answer(map) / seed
+                else -> root.operation.reversed().function(
+                    seed,
+                    left.answer(map)
+                )
+            }
+            return solvePartTwo(right, newSeed, map)
+        }
     }
 
 
-    class MonkeyVal private constructor(
+    private class MonkeyVal private constructor(
         val name: String,
         val number: Long?,
         val monkey1: String?,
@@ -141,74 +139,39 @@ class MonkeyMath : Task {
         )
 
         companion object {
-            val humanName = "humn"
+            const val humanName = "humn"
         }
 
         fun left(map: MutableMap<String, MonkeyVal>) = map[monkey1]
         fun right(map: MutableMap<String, MonkeyVal>) = map[monkey2]
 
         fun isHumanInSubtree(map: MutableMap<String, MonkeyVal>): Boolean {
-            if (this.name == humanName) {
-                return true
+            return if (this.name == humanName) {
+                true
             } else if (this.number != null) {
-                return false
+                false
             } else {
-                return left(map)!!.isHumanInSubtree(map) || right(map)!!.isHumanInSubtree(map)
+                left(map)!!.isHumanInSubtree(map) || right(map)!!.isHumanInSubtree(map)
             }
         }
 
         fun answer(map: MutableMap<String, MonkeyVal>): Long {
-            return if (number != null) {
-                number
-            } else {
-                operation!!.function(map[monkey1]!!.answer(map), map[monkey2]!!.answer(map))
-            }
+            return number
+                ?: operation!!.function(map[monkey1]!!.answer(map), map[monkey2]!!.answer(map))
         }
-
-//        fun reversedAnswer(map: MutableMap<String, MonkeyVal>): Long {
-//            return if (number != null) {
-//                number
-//            } else {
-//                operation!!.reversed().function(map[monkey1]!!.answer(map), map[monkey2]!!.answer(map))
-//            }
-//        }
-//
-//
-//        fun textAnswer(map: MutableMap<String, MonkeyVal>): String {
-//            return if (name == humanName) {
-//                humanName
-//            } else if (number != null) {
-//                number.toString()
-//            } else {
-//                "(" + map[monkey1]!!.textAnswer(map) + operation!!.symbol + map[monkey2]!!.textAnswer(map) + ")"
-//            }
-//        }
-//
-//        fun reversedTextAnswer(map: MutableMap<String, MonkeyVal>): String {
-//            return if (name == humanName) {
-//                humanName
-//            } else if (number != null) {
-//                number.toString()
-//            } else {
-//                "(" + map[monkey1]!!.reversedTextAnswer(map) + operation!!.reversed().symbol + map[monkey2]!!.reversedTextAnswer(
-//                    map
-//                ) + ")"
-//            }
-//        }
-
 
     }
 
-    enum class Operation(val symbol: Char, val function: (Long, Long) -> Long) {
-        ADDITION('+', { a, b -> a + b }),
-        SUBSTRACTION('-', { a, b -> a - b }),
-        MULTIPLICATION('*', { a, b -> a * b }),
-        DIVISION('/', { a, b -> a / b });
+    private enum class Operation(val function: (Long, Long) -> Long) {
+        ADDITION({ a, b -> a + b }),
+        SUBTRACTION({ a, b -> a - b }),
+        MULTIPLICATION({ a, b -> a * b }),
+        DIVISION({ a, b -> a / b });
 
         fun reversed(): Operation {
             return when (this) {
-                ADDITION -> SUBSTRACTION
-                SUBSTRACTION -> ADDITION
+                ADDITION -> SUBTRACTION
+                SUBTRACTION -> ADDITION
                 MULTIPLICATION -> DIVISION
                 DIVISION -> MULTIPLICATION
             }
